07d948ff024b8b822eec9d1debdda6bfef5813e7
Kareena <angewills387@gmail.com>
2025-06-01 14:05:29 +0545

    work with drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,26 @@
+import * as dotenv from "dotenv"
+import { defineConfig } from 'drizzle-kit';
+
+
+dotenv.config({path: ".env.local"}) //developing application local, we can ignore .local otherwise
+
+if (!process.env.DATABSE_URL){
+    throw new Error ("Database url is not set in .env.local");
+}
+
+export default defineConfig({
+    schema: './lib/db/schema.ts',
+    out: './drizzle',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+  },
+
+  migrations: {
+    table: "_drizzle_migration",
+    schema: "public"
+  },
+
+  verbose: true,
+  strict: true,
+});
