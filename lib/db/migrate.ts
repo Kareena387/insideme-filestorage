1f92385e33c2b2510b6cbe72c8b239aace7b8ac9
Kareena <angewills387@gmail.com>
2025-06-01 15:17:31 +0545

    migration

---
+++ lib/db/migrate.ts
@@ -0,0 +1,29 @@
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" }); //developing application local, we can ignore .local otherwise
+
+if (!process.env.DATABSE_URL) {
+    throw new Error("Database url is not set in .env.local");
+}
+
+async function runMigration() {
+    try {
+        const sql = neon(process.env.DATABASE_URL!);
+        const db = drizzle(sql);
+
+        await migrate(db, { migrationsFolder: "./drizzle" });
+        console.log("All migrations are successfully done");
+
+    } catch (error) {
+        console.log("All migrations are successfully done");
+        process.exit(1);
+
+    }
+
+}
+
+runMigration();
