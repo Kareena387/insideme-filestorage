ac94b48ef9434f22eb65e387952b7144a88cbb78
Kareena <angewills387@gmail.com>
2025-06-01 15:23:48 +0545

    changes indexfile

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -5,7 +5,13 @@ import { drizzle } from "drizzle-orm/neon-http";
 import {neon} from "@neondatabase/serverless";
 
 
-import * as schema from "./schema"
+import * as schema from "./schema";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local"}); // optional, only if needed here
+
+if (!process.env.DARABASE_URL) {
+    throw new Error("Missing DATABSE_URL in environment");
+}
 
 const sql = neon(process.env.DATABASE_URL!)
 
