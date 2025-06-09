ba1076ffddf9c853e270cba31828421fd4bdf31e
Kareena <angewills387@gmail.com>
2025-06-09 17:32:22 +0545

    adding SignUpSchema

---
+++ schemas/signUpSchema.ts
@@ -0,0 +1,22 @@
+import * as z from "zod";
+export const signUpSchema = z
+    .object({
+        email: z
+            .string()
+            .min(1, { message: "Email is required" })
+            .email({ message: "Please enter a valid email" }),
+        password: z
+            .string()
+            .min(1, { message: "Password is required" })
+            .min(8, { message: "password should be minimum of 8 characters" }),
+        passwordConfirmation: z
+            .string()
+            .min(1, { message: "Please confirm your password" }),
+    })
+    .refine((data) => data.password === data.
+        passwordConfirmation, {
+        message: "Password don not match",
+        path: ["passwordConfirmation"],
+
+    })
+
